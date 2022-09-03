aa78ff16481cd509324c9040af0d866553900793
jfeliz <jorge.feliz42@gmail.com>
2022-09-02 21:38:24 -0400

    Articulos tienda

--- ProyectoWeb/tienda/models.py
+++ ProyectoWeb/tienda/models.py
@@ -22,6 +22,11 @@ class Producto(models.Model):
     imagen = models.ImageField(upload_to = 'tienda', null=True, blank=True)
     precio = models.FloatField()
     disponibilidad = models.BooleanField(default=True)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
 
     class Meta:
         verbose_name = 'Producto'
